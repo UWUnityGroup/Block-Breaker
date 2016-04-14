929294f70418025e359a6c8e7aa2c7cbf4fb6569
Daeyoun Kim <Daeyoun Kim>
2016-04-14 12:51:36 -0500

    Added a singleton music player

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+    static MusicPlayer INSTANCE = null;
+
+	// Use this for initialization
+	void Start () {
+        if (INSTANCE != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            INSTANCE = this;
+            DontDestroyOnLoad(gameObject);
+        }        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
